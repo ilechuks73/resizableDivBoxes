1be196f6b957a1da95b71e60a470966c58f5c47e
ilechuks73 <62202169+ilechuks73@users.noreply.github.com>
2020-12-27 15:51:50 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+var box1 = document.getElementById('box1')
+var box2 = document.getElementById('box2')
+var box3 = document.getElementById('box3')
+
+
+
+function dragAlert(i){
+
+    if(i=='1'){
+        box2.style.left = event.clientX+'px'
+        box1.style.right = window.innerWidth - event.clientX+'px'
+    }
+
+    if(i=='2'){
+        box2.style.right = window.innerWidth - event.clientX+'px'
+        box3.style.left = event.clientX+'px'
+    }
+}
+
+
