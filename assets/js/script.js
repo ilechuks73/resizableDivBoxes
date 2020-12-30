a4afa143096a1e82aa0776f9a85c91d7d0a2f0d4
ilechuks73 <ilechuks73@gmail.com>
2020-12-30 01:12:01 +0100

    firefox fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,13 @@ var box3 = document.getElementById('box3')
 function dragAlert(i){
 
     if(i=='1'){
-        box2.style.left = event.screenX+'px'
-        box1.style.right = window.innerWidth - event.screenX+'px'
+        box2.style.left = event.clientX+'px'
+        box1.style.right = window.innerWidth - event.pageX+'px'
     }
 
     if(i=='2'){
-        box2.style.right = window.innerWidth - event.screenX+'px'
-        box3.style.left = event.screenX+'px'
+        box2.style.right = window.innerWidth - event.pageX+'px'
+        box3.style.left = event.clientX+'px'
     }
 }
 
